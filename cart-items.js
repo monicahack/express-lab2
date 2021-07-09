cbb4f70114ddb914f0417f81abb344afd87246fb
Monica Hackney <mhackney@HAGERTY.com>
2021-07-08 22:10:16 -0400

    updates

--- cart-items.js
+++ cart-items.js
@@ -1,17 +1,58 @@
 const express = require("express");
 const cart = express.Router();
+const pool = require("./pg-connection-pool");
 
-const cartItems = [
-    {id: 1, product: "Shirt", price: 20.99, quantity: 12},
-    {id: 2, product: "Shorts", price: 24.79, quantity: 9},
-    {id: 3, product: "Socks", price: 3.50, quantity: 35},
-    {id: 4, product: "Shoes", price: 37.00, quantity: 15},
-    {id: 5, product: "Hat", price: 15.99, quantity: 7},
-    {id: 6, product: "Fancy Hat", price: 18.99, quantity: 14},
-    {id: 7, product: "Fanny Pack", price: 12.00, quantity: 2},
-    {id: 8, product: "Backpack", price: 35.00, quantity: 27},
-]  
+async function getTable(req, res) {
+    try {
+    const results = await pool.query(
+        `select * from shopping_cart`, 
+    )
+      res.json(results.rows);
+    } catch (error) {
+        console.log(error);
+        res.json('ERROR');
+      }
+    }
 
+  cart.get("/", async (req, res) => {
+    let price = parseFloat(req.query.maxPrice);
+    let product = req.query.prefix;
+    let pageSize = req.query.pageSize;
+    if(price){
+        let results = await pool.query('SELECT * FROM shopping_cart WHERE price <= $1', [price]);
+        res.json(results.rows);
+    }
+    if(product){
+        let results = await pool.query('SELECT * FROM shopping_cart WHERE product ILIKE $1', [product + '%']);
+        res.json(results.rows);
+    }
+    if(pageSize){
+        let results = await pool.query('SELECT * FROM shopping_cart LIMIT $1', [pageSize]);
+        res.json(results.rows);
+    } else {
+        getTable(req, res); 
+    }
+});
+
+cart.get("/:id", async (req, res) => {
+    let id = parseFloat(req.params.id);
+    let results = await pool.query('SELECT * FROM shopping_cart WHERE id=$1',[id])
+    if (results.rows.length === 0){
+        res.send(404);
+    }  else {
+        res.json(results.rows);        
+    }  
+});
+
+cart.post("/", async (req, res) => {
+    let results = await pool.query('INSERT INTO shopping_cart (product, price, quantity) values ($1, $2, $3) RETURNING *', [req.body.product, req.body.price, req.body.quantity])
+    res.status(201).json(results.rows);
+});
+
+cart.delete("/:id", async (req, res) => {
+    let results = await pool.query('DELETE FROM shopping_cart WHERE id=$1',[req.params.id])
+    res.status(204).json("Deleting a cart item..");
+  });
 
 
 module.exports = cart;
